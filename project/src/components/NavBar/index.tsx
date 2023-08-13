93ae96d6c28032233f7472d7adab7e3edd978b69
@dmathcore231 <dmathcore231@gmail.com>
2023-08-13 13:29:49 +0300

    feat(header): finished navBar template

--- project/src/components/NavBar/index.tsx
+++ project/src/components/NavBar/index.tsx
@@ -2,13 +2,21 @@ import { SearchInput } from "../SearchInput"
 
 export function NavBar(): JSX.Element {
   return (
-    <nav className="nav-bar d-flex justify-content-between">
+    <nav className="nav-bar d-flex justify-content-between align-items-center pt-4 pb-4 border-bottom ">
       <div className="logo">
         <img className="logo__img img-fluid" src="./src/images/logo.png" alt="logo" />
       </div>
       <SearchInput />
-      <div className="nav-bar__icon-group">
-        <img className="img-fluid" src="" alt="" />
+      <div className="icon-group d-flex ">
+        <a href="#" className="icon-group__item p-3">
+          <img className="icon-group__img img-fluid" src="./src/images/heart-icon.png" alt="" />
+        </a>
+        <a href="#" className="icon-group__item p-3">
+          <img className="icon-group__img img-fluid" src="./src/images/shop-bag.png" alt="" />
+        </a>
+        <a href="#" className="icon-group__item p-3">
+          <img className="icon-group__img img-fluid" src="./src/images/user-icon.png" alt="" />
+        </a>
       </div>
     </nav>
   )
--- project/src/components/SearchInput/index.tsx
+++ project/src/components/SearchInput/index.tsx
@@ -1,11 +1,9 @@
 
 export function SearchInput(): JSX.Element {
   return (
-    <form className="search-input d-flex gap-2">
+    <form className="search-input d-flex gap-2 align-items-center w-50">
       <input type="text" className="form-control" placeholder="Search" />
-      <div className="search-input__icon pt-1">
-        <img className="search-input__img" src="./src/images/Icon-Search.png" alt="" />
-      </div>
+      <img className="search-input__img" src="./src/images/Icon-Search.png" alt="" />
     </form>
   )
 }
